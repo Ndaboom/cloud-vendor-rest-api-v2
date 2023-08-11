e130478d00e00ea7bd05294ff9446aa1d107af4f
Sammy Ndabo <ndabosam084@gmail.com>
2023-08-11 19:09:40 +0200

    DB Connection

---
+++ src/main/java/com/cyberclick/restapiv2/controllers/CloudVendorAPIService.java
@@ -0,0 +1,39 @@
+package com.cyberclick.restapiv2.controllers;
+
+
+import org.springframework.web.bind.annotation.*;
+import com.cyberclick.restapiv2.models.CloudVendor;
+
+@RestController
+@RequestMapping("/cloudvendor")
+public class CloudVendorAPIService {
+    CloudVendor cloudVendor;
+    @GetMapping("{vendorId}")
+    public CloudVendor getCloudVendorDetails(String vendorId){
+        return cloudVendor;
+//        return new CloudVendor("C1","Vendor 1", "Address One",
+//                "XXX-XXX-XXX");
+    }
+
+    @PostMapping
+    public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor)
+    {
+        this.cloudVendor = cloudVendor;
+        return "Cloud Vendor Created Successfully";
+    }
+
+    @PutMapping
+    public String updateCloudVendorDetails(@RequestBody CloudVendor cloudVendor)
+    {
+        this.cloudVendor = cloudVendor;
+        return "Cloud Vendor Updated Successfully";
+    }
+
+    @DeleteMapping("{vendorId}")
+    public String deleteCloudVendorDetails(String vendorId)
+    {
+        this.cloudVendor = null;
+        return "Cloud Vendor Deleted Successfully";
+    }
+}
+
---
+++ src/test/java/com/cyberclick/restapiv2/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyberclick.restapiv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
