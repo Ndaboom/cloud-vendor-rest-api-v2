8010fcd49853d3804f29b5fa434ca3c5eccae17b
Sammy Ndabo <ndabosam084@gmail.com>
2023-08-11 19:55:03 +0200

    Repository layer implemented

---
+++ src/main/java/com/cyberclick/restapiv2/repository/CloudVendorRepository.java
@@ -0,0 +1,8 @@
+package com.cyberclick.restapiv2.repository;
+
+import com.cyberclick.restapiv2.models.CloudVendor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CloudVendorRepository extends JpaRepository<CloudVendor, String> {
+
+}
