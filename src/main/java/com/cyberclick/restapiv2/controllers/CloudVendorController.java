ead53dced6941877d31cf5090369aa146d0ceb10
Sammy Ndabo <ndabosam084@gmail.com>
2023-08-11 21:55:37 +0200

    Implement controller CRUD and GetAll methods

---
+++ src/main/java/com/cyberclick/restapiv2/controllers/CloudVendorController.java
@@ -0,0 +1,49 @@
+package com.cyberclick.restapiv2.controllers;
+
+
+import com.cyberclick.restapiv2.service.CloudVendorService;
+import org.springframework.web.bind.annotation.*;
+import com.cyberclick.restapiv2.models.CloudVendor;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cloudvendor")
+public class CloudVendorController {
+    CloudVendorService cloudVendorService;
+    public CloudVendorController(CloudVendorService cloudVendorService) {
+        this.cloudVendorService = cloudVendorService;
+    }
+
+    @GetMapping("{vendorId}")
+    public CloudVendor getCloudVendorDetails(@PathVariable("vendorId") String vendorId){
+        return cloudVendorService.getCloudVendor(vendorId);
+    }
+
+    @GetMapping()
+    public List<CloudVendor> getAllCloudVendorDetails(){
+        return cloudVendorService.getAllCloudVendors();
+    }
+
+    @PostMapping
+    public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor)
+    {
+        cloudVendorService.createCloudVendor(cloudVendor);
+        return "Cloud Vendor Created Successfully";
+    }
+
+    @PutMapping
+    public String updateCloudVendorDetails(@RequestBody CloudVendor cloudVendor)
+    {
+        cloudVendorService.updateCloudVendor(cloudVendor);
+        return "Cloud Vendor Updated Successfully";
+    }
+
+    @DeleteMapping("{vendorId}")
+    public String deleteCloudVendorDetails(@PathVariable("vendorId") String vendorId)
+    {
+        cloudVendorService.deleteCloudVendor(vendorId);
+        return "Cloud Vendor Deleted Successfully";
+    }
+}
+
--- src/main/java/com/cyberclick/restapiv2/service/CloudVendorService.java
+++ src/main/java/com/cyberclick/restapiv2/service/CloudVendorService.java
@@ -11,6 +11,7 @@ public interface CloudVendorService {
 
     public String deleteCloudVendor(String cloudVendorId);
 
-    public CloudVendor getCloudVendor();
+    CloudVendor getCloudVendor(String cloudVendorId);
+
     public List<CloudVendor> getAllCloudVendors();
 }
